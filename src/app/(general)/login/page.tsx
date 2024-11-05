4da5e5fe50ec8aab3e16ebed5371716d5781a332
KevinVincent016 <kevinvincentvl016@gmail.com>
2024-11-05 13:06:46 -0500

    Add new componentes, Add Login Page, Mod Navbar Login, Register buttons

--- src/app/(general)/login/page.tsx
+++ src/app/(general)/login/page.tsx
@@ -3,6 +3,11 @@
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useLogin } from "@/hooks/auth/useLogin";
+import React from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import Link from "next/dist/client/link";
 
 export default function usernamePage(){
     const [username, setUsername] = useState(""); 
@@ -22,25 +27,80 @@ export default function usernamePage(){
                     alert("Invalid Credentials")})
         }
     }
+
+    const handleSubmit = () => {
+
+    }
     
     return (
-        <div className="flex flex-col items-center justify-center w-full h-full">
-            <h1>Login Page</h1>
-            <label className="mt-4">username</label>
-            <input type="text" 
-            className="w-80 h-8 px-2 border border-solid border-block rounded text-black" 
-            value={username} onChange={(e)=>setUsername(e.target.value)}/>
-            <label className="mt-4">Password</label>
-            <input type="password" 
-            className="w-80 h-8 px-2 border border-solid border-block rounded text-black" 
-            value={password} onChange={(e)=>setPassword(e.target.value)}/>
-
-            <button 
-            className="mt-4 p-2 bg-gray-900 text-white rounded hover:bg-gray-600 transition-all"
-            onClick={onSubmit}>
-                username
+        <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black my-8 md:my-16">
+          <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+            Welcome to PetSociety
+          </h2>
+          <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+            Inicia sesión para continuar
+          </p>
+     
+          <form className="my-8" onSubmit={handleSubmit}>
+            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+              <LabelInputContainer>
+                <Label htmlFor="username">Nombre de usuario</Label>
+                <Input id="username" placeholder="Username" type="text" value={username} onChange={(e)=>setUsername(e.target.value)} />
+              </LabelInputContainer>
+            </div>
+            <LabelInputContainer className="mb-4">
+              <Label htmlFor="password">Contraseña</Label>
+              <Input id="password" placeholder="••••••••" type="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+            </LabelInputContainer>
+     
+            <button
+              className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+              type="submit"
+              onClick={onSubmit}
+            >
+              Iniciar sesión &rarr;
+              <BottomGradient />
             </button>
+     
+            <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+     
+            <div className="flex flex-col space-y-4">
+                <Link href="/register">
+                    <button
+                        className=" relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                    >
+                    <span className="text-neutral-700 dark:text-neutral-300 text-sm text-center">
+                        Registrarse
+                    </span>
+                    <BottomGradient />
+                    </button>
+                </Link>
+            </div>
 
+          </form>
         </div>
-    )
+    );
 }
+
+const BottomGradient = () => {
+    return (
+      <>
+        <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+        <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+      </>
+    );
+  };
+   
+  const LabelInputContainer = ({
+    children,
+    className,
+  }: {
+    children: React.ReactNode;
+    className?: string;
+  }) => {
+    return (
+      <div className={cn("flex flex-col space-y-2 w-full", className)}>
+        {children}
+      </div>
+    );
+  };
--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -2,6 +2,7 @@ import HomeIcon from "@/media/pata.png";
 import Link from "next/link";
 import { ActiveLink } from "../active-link/ActiveLink";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
 const navItems = [
     {name:'Inicio', path: '/'},
@@ -20,12 +21,22 @@ export const Navbar = () => {
                 <ActiveLink  key={item.path} {...item}/>
             ))
         }
-        <div className="flex">
+        <div className="flex flex-row space-x-2 items-center px-2">
             <Link href="/login">
-                <button className="p-2 m-2 text-black text-xs bg-gray-300 rounded-xl border-2 border-gray-600">Iniciar sesión</button>
+                <button
+                    className="relative group/btn flex space-x-2 items-center justify-center px-4 text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                >
+                <span className="text-neutral-700 dark:text-neutral-300 text-sm text-center">
+                    Iniciar sesión
+                </span>
+                </button>
             </Link>
             <Link href="/register">
-                <button className="p-2 m-2 text-white text-xs bg-gray-900 rounded-xl border-2 border-gray-800">Registrarse</button>
+                <button
+                className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] px-4"
+                >
+                    Registrarse
+                </button>
             </Link>
         </div>
       </nav>
